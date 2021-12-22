1d8b3cc30890c1e42ab6a9d401014dea9e900007
Adam <barcelona1999.aw@gmail/com>
2021-12-22 14:35:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,38 +3,147 @@ function myFunction() {
   let x = document.getElementById("numb").value;
   
   let text;
-  if (x != "Test_Belbin_@01"
-    //isNaN(x) || x < 1 || x > 10
-    ) {
+  if (x == "Test_Belbin_@01") {
 
-    //text = "Input not valid";
+      text =  "https://rafal-dabek.github.io/Test_Belbina/";
 
-    //document.getElementById("demo").innerHTML = text;
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+    
+  } 
+  else if(x == "Test_Komunikacja_@02")
+  {
+
+    text = "https://rafal-dabek.github.io/Test_na-style_komunikacji/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+
+  }
+  else if(x == "Test_Lasi_@03")
+  {
+
+      text = "https://rafal-dabek.github.io/Test_Lasi/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_Stres_@04")
+  {
+
+      text = "https://rafal-dabek.github.io/Stres_test/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_Oldham_@05")
+  {
+
+      text = "https://rafal-dabek.github.io/Test_Oldhama/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_Kultura_@06")
+  {
+
+      text = "https://rafal-dabek.github.io/Diagnozowanie_kultury_organizacji/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_McClelland_@07")
+  {
+
+      text = "https://rafal-dabek.github.io/Test_McClellanda/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_Potrzeby_@08")
+  {
+
+      text = "https://rafal-dabek.github.io/Test_potrzeby_podw-adnych/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_Konflikt_@09")
+  {
+
+      text = "https://rafal-dabek.github.io/test_na_konfliktowo-/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_MotywacyjneDNA_@10")
+  {
+
+      text = "https://rafal-dabek.github.io/Motywacyjne-Dna/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else if(x == "Test_StylCoaching_@11")
+  {
+
+      text = "https://rafal-dabek.github.io/Formularz/";
+    
+      document.getElementById("demo").style.display = "none"
+  
+      document.getElementById("demo2").style.display="block";
+    
+      document.getElementById("demo2").setAttribute("href",text);
+      
+  }
+  else
+  {
 
     document.getElementById("demo").style.display="block";
 
     document.getElementById("demo2").style.display="none";
-    
-  } 
-  else
-  {
 
-    text =  "https://rafal-dabek.github.io/Test_Belbina/";
-
-    
-    document.getElementById("demo").style.display = "none"
-
-    document.getElementById("demo2").style.display="block";
-  
-    document.getElementById("demo2").setAttribute("href",text);
-    
   }
-  //document.getElementById("demo").innerHTML = text;
+
 }
 
-
-
-/*if (document.getElementById("p").style.display == "none"){
-  document.getElementById("p").style.display="block";
-}*/
 
