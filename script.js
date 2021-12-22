70704bca528d12fecd1c25bdfbe578e04c81339b
Adam <barcelona1999.aw@gmail/com>
2021-12-22 13:38:06 +0100

    Update

--- script.js
+++ script.js
@@ -3,12 +3,33 @@ function myFunction() {
   let x = document.getElementById("numb").value;
   
   let text;
-  if (isNaN(x) || x < 1 || x > 10) {
-    text = "Input not valid";
-  } else {
-    text = "Link w opisie";
+  if (x != "Test_Belbin_@01"
+    //isNaN(x) || x < 1 || x > 10
+    ) {
+
+    //text = "Input not valid";
+
+    //document.getElementById("demo").innerHTML = text;
+
+    document.getElementById("demo").style.display="block";
+
+    document.getElementById("demo2").style.display="none";
+    
+  } 
+  else
+  {
+
+    text =  "https://rafal-dabek.github.io/Test_Belbina/";
+
+    
+    document.getElementById("demo").style.display = "none"
+
+    document.getElementById("demo2").style.display="block";
+  
+    document.getElementById("demo2").setAttribute("href",text);
+    
   }
-  document.getElementById("demo").innerHTML = text;
+  //document.getElementById("demo").innerHTML = text;
 }
 
 
